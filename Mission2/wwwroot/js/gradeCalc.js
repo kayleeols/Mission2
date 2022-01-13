60363878f6fa7a0f9b18ef6bfc6dea14084d608c
kayleeols <kayleeols@yahoo.com>
2022-01-12 23:55:56 -0700

    Add project files.

---
+++ Mission2/wwwroot/js/gradeCalc.js
@@ -0,0 +1,12 @@
+﻿$("#btnSend").click(function () {
+
+    var a = ($("#assign".val()))
+    var g = ($("#group").val())
+    var qu = ($("#quiz").val())
+    var ex = ($("#exam").val())
+    var intex = ($("#intex").val())
+
+    var output = (a * .55) + (g * .05) + (qu * .1) + (ex * .2) + (intex * .1)
+
+    alert("Your Final Grade in IS 413 is : " + output + "%");
+})
